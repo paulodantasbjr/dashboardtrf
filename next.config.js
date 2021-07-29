31413018edf420f826f2d8c1ab56c9d22837451a
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-07-29 18:00:17 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
-    BASE_URL: 'http://localhost:3000',
+    BASE_URL: 'https://dashboardtrf.vercel.app',
     MONGODB_URL:
       'mongodb+srv://userTRFDB:userTRFDB@cluster0.wl4ld.mongodb.net/dashboardtrf?retryWrites=true&w=majority',
-    ACCESS_TOKEN_SECRET: 'YOUR_ACCESS_TOKEN_SECRET',
-    REFRESH_TOKEN_SECRET: 'YOUR_REFRESH_TOKEN_SECRET',
+    ACCESS_TOKEN_SECRET: 'MEU_TOKEN_SECRETO',
+    REFRESH_TOKEN_SECRET: 'MEU_REFRESH_TOKEN_SECRETO',
   },
 }
