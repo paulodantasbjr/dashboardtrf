3c9d31414b08440d22314f5c78ef114490c4f138
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-07-29 18:57:34 -0300

    cors test7

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ module.exports = {
       {
         // matching all API routes
         source: '/api/:path*',
-        destination: 'https://dashboardtrf.vercel.app/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
